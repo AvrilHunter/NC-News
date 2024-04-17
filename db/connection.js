fa9e8fd0763a5e0adf729aedc148b49e74f0f73a
AvrilHunter <avrilhunter15@gmail.com>
2024-04-17 11:02:02 +0100

    hosting amends

--- db/connection.js
+++ db/connection.js
@@ -6,15 +6,6 @@ require('dotenv').config({
 });
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-<<<<<<< HEAD
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
-
-const config = {};
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-=======
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
@@ -22,7 +13,6 @@ const config = {}
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL
   config.max=2
->>>>>>> d4556149e8543c16f954d917d2044b69ccbce7e3
 }
 
 module.exports = new Pool(config);
