eb427ffe69689182391bde25c8902e6069b3e288
AvrilHunter <avrilhunter15@gmail.com>
2024-04-17 11:07:39 +0100

    amends for hosting

--- listen.js
+++ listen.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 const app = require("./app.js");
 const { PORT = 9090 } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
 });
-=======
-const app = require("./app.js")
-const { PORT = 9090 } = process.env
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}...`)
-})
->>>>>>> d4556149e8543c16f954d917d2044b69ccbce7e3
