bc0ee095f1820ce8fa26704d1517e0e9316ed731
AvrilHunter <avrilhunter15@gmail.com>
2024-04-17 10:58:22 +0100

    updates for hosting

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -8,7 +8,6 @@ const { doesTopicExist } = require("../models/topic.model");
 
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
-  console.log(article_id);
  return selectArticle(article_id)
     .then((article) => {
       res.status(200).send({ article });
