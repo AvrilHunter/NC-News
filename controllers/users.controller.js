4e75d1399a2f2776c65e0058c814ee8ea3c327bb
AvrilHunter <avrilhunter15@gmail.com>
2024-04-18 11:50:45 +0100

    17. users/usernames Can search by usernames deals with errors testing complete JSON completed

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,15 @@
-const {selectUsers} = require("../models/users.model")
+const { use } = require("../app")
+const { selectUsers, selectUserByUsername } = require("../models/users.model");
 
 exports.getUsers = (req,res,next) => {
   return selectUsers().then((users) => {
     res.status(200).send({users})
   })
+}
+
+exports.getUserByUsername = (req, res, next) => {
+  const { username } = req.params
+  return selectUserByUsername(username).then((user) => {
+    res.status(200).send({user})
+  }).catch(next)
 }
