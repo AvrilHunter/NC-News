80333de918dc5cfabb78694929b4cd8bc2e5dbf7
AvrilHunter <avrilhunter15@gmail.com>
2024-06-12 16:53:05 +0100

    refactor: Async/await for User and Topic models

--- models/topic.model.js
+++ models/topic.model.js
@@ -1,44 +1,36 @@
 const db = require("../db/connection");
 
-exports.selectTopics = () => {
-  return db.query(`SELECT slug, description FROM topics;`).then(({ rows }) => {
-    return rows;
-  });
+exports.selectTopics = async function () {
+  const topics = await db.query(`SELECT slug, description FROM topics;`);
+  return topics.rows;
 };
 
-exports.doesTopicExist = (topic) => {
+exports.doesTopicExist = async function (topic) {
   if (!topic) {
     return Promise.resolve;
   }
-
-  return db
-    .query(
-      `SELECT * 
+  const selectedTopic = await db.query(
+    `SELECT * 
   FROM topics
   WHERE slug=$1`,
-      [topic]
-    )
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, message: "topic not found" });
-      } else {
-        return rows;
-      }
-    });
+    [topic]
+  );
+  if (selectedTopic.rows.length === 0) {
+    return Promise.reject({ status: 404, message: "topic not found" });
+  } else {
+    return selectedTopic.rows;
+  }
 };
 
-exports.insertTopic = (description, slug) => {
-  return db
-    .query(
-      `INSERT INTO topics
+exports.insertTopic = async function (description, slug) {
+  const topicAdded = await db.query(
+    `INSERT INTO topics
 (slug, description)
 VALUES
 ($1, $2)
 RETURNING *;
 `,
-      [slug, description]
-    )
-    .then(({ rows }) => {
-      return rows[0];
-    });
+    [slug, description]
+  );
+  return topicAdded.rows[0];
 };
